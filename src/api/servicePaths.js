cc89769005018eaa51efad7e1fdecb7f17666ad1
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2017-05-06 18:20:09 -0500

    added questions and related words API paths

--- src/api/servicePaths.js
+++ src/api/servicePaths.js
@@ -1,7 +1,11 @@
-export default {
+export const QUESTIONS_PATHS = {
   BASE_URL: 'http://jservice.io',
   RANDOM_QUESTION: '/api/random',
   CATEGORIES: '/api/categories',
   CATEGORY: '/api/category',
   INVALID: '/api/invalid',
+};
+
+export const WORDS_PATHS = {
+  BASE_URL: 'http://api.datamuse.com/words',
 };
