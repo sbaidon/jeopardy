bed4194ba0c0227aab7958ba206a0550dc3608f5
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2017-05-06 22:40:33 -0500

    Clean up QuestionCard component

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,93 +1,37 @@
 <template>
   <div class="question">
-    <md-card  @click.native.once="openDialog(id)" class="question" :class="{ 'md-warn': wasClicked }">
+    <md-card  @click.native.once="activeQuestion()" class="question" :class="{ 'md-warn': wasClicked }">
         <md-card-content>
           {{ points }}
         </md-card-content>
       </md-card>
-
-    <md-dialog :ref="id"  @open="onOpen" @close="onClose">
-      <md-dialog-title v-text="dialogTitle"></md-dialog-title>
-
-      <md-dialog-content v-text="question"></md-dialog-content>
-
-      <md-dialog-actions>
-        <form>
-          <md-radio v-for="(option, index) in options" key="index" v-model="response" name="test" :md-value="option">{{ option }} </md-radio>
-        </form>
-        <md-button class="md-primary" @click.native="closeDialog(id)">Ok</md-button>
-         <timer></timer>
-      </md-dialog-actions>
-    </md-dialog>
   </div>
 </template>
 
 <script>
-import { mapActions, mapMutations, mapState, mapGetters } from 'vuex';
 import Timer from './Timer';
 
 export default {
-  props: ['points', 'answer', 'category', 'question', 'id'],
+  props: ['points', 'answer', 'category', 'question', 'id', 'options', 'onClick'],
   name: 'question-card',
   components: { Timer },
   data() {
     return {
-      response: '',
       wasClicked: false,
     };
   },
-  computed: {
-    ...mapState(['timer', 'secondOption', 'thirdOption']),
-    ...mapGetters(['getOptions']),
-    dialogTitle() {
-      return `To win ${this.points}`;
-    },
-    options() {
-      return this.shuffle([...this.getOptions, this.answer]);
-    },
-  },
-  mounted() {
-
-  },
   methods: {
-    ...mapActions(['advanceTurn', 'checkAnswer', 'getRelatedWord']),
-    ...mapMutations(['ADVANCE_TIMER', 'RESET_TIMER']),
-    shuffle(a) {
-      /* eslint-disable */
-      for (let i = a.length; i; i--) {
-        let j = Math.floor(Math.random() * i);
-        [a[i - 1], a[j]] = [a[j], a[i - 1]];
-      }
-      /* eslint-enable */
-      return a;
-    },
-    openDialog(ref) {
-      this.getRelatedWord({ word: this.answer, option: 'secondOption' });
-      this.getRelatedWord({ word: this.answer, option: 'thirdOption' });
-      this.$refs[ref].open();
-    },
-    closeDialog(ref) {
-      this.$refs[ref].close();
-    },
-    onOpen() {
-      this.wasClicked = true;
-      this.interval = setInterval(() => {
-        if (this.timer <= 0) {
-          window.clearInterval(this.interval);
-          this.closeDialog(this.id);
-        }
-        this.ADVANCE_TIMER();
-      }, 1000);
-    },
-    onClose() {
-      window.clearInterval(this.interval);
-      this.RESET_TIMER();
-      this.checkAnswer({
-        answer: this.response,
-        realAnswer: this.answer,
+    activeQuestion() {
+      const question = {
+        answer: this.answer,
+        category: this.category,
+        options: this.options,
+        id: this.id,
+        question: this.question,
         points: this.points,
-      });
-      this.advanceTurn();
+      };
+      this.wasClicked = true;
+      this.onClick(question);
     },
   },
 };
